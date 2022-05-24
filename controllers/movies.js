c628ca683f8fed9f599fbcfc8cc171c6fe1c6dab
Amidamaru <hoztovari99@yandex.ru>
2022-05-24 23:54:12 +0300

    230522-8

--- controllers/movies.js
+++ controllers/movies.js
@@ -2,20 +2,25 @@ const Movie = require('../models/movie');
 const { moviesAdapter } = require('../utuls/muviesAdapter');
 const NotFoundError = require('../errors/NotFoundError');
 const DeletionError = require('../errors/DeletionError');
+const BadRequestError = require('../errors/BadRequestError');
 
-exports.createMove = (req, res) => {
+exports.createMove = (req, res, next) => {
   const userId = req.user && req.user._id;
   const movieData = { ...moviesAdapter(req.body), owner: userId };
 
   Movie.create(movieData)
     .then((movie) => res.send(movie))
     .catch((err) => {
-      console.log(err);
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError('Переданы некорректные данные при создании карточки фильма'));
+      } else {
+        next(err);
+      }
     });
 };
 
-exports.deleteMovieById = (req, res) => {
-  //const { id: movieId } = req.params;
+exports.deleteMovieById = (req, res, next) => {
+  console.log('kjkj');
   const userId = req.user && req.user._id;
 
   Movie.findById(req.params._id)
@@ -31,16 +36,20 @@ exports.deleteMovieById = (req, res) => {
       throw new DeletionError('Нельзя удалять чужой фильм');
     })
     .catch((err) => {
-      res.status(err.statusCode).send(err);
+      if (err.name === 'CastError') {
+        next(new BadRequestError('Не валидный id'));
+      } else {
+        next(err);
+      }
     });
 };
 
-exports.getMovie = (req, res) => {
+exports.getMovie = (req, res, next) => {
   const userId = req.user && req.user._id;
 
   Movie.find({ owner: userId })
     .then((cards) => res.send(cards))
     .catch((err) => {
-      console.log(err);
+      next(err);
     });
 };
